8e3f467f51f991c0db5b76d5368492e68ea7ce58
evodychko <157400289+evodychko@users.noreply.github.com>
2025-09-08 23:51:48 -0400

    Create prototype0.js

---
+++ prototype0.js
@@ -0,0 +1,29 @@
+export const cards = [
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },        
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },        
+  {
+    "name": "xxx",
+    "definition": "xxx",
+    "failureMode": "xxx",
+    "example": "xxx"
+  },        
+  //comma to separate objects in the array
+
+//reproduce the above structure for the other four
+];
